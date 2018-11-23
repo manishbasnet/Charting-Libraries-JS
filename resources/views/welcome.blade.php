81c576fd075ba5295f05a737220cea866ccee12f
Manish Basnet <manish.basnetnp@gmail.com>
2018-11-23 13:51:33 +0545

    global options

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -101,6 +101,11 @@
 
         <div id="container" style="height: 400px; min-width: 310px"></div>
 
+        <br>
+        <hr>
+
+        <div id="container2" style="height: 400px; min-width: 310px"></div>
+
         <script>
 
             // Bad code:
@@ -152,9 +157,54 @@
 
 
             /* Set options globally */
-            
+            $(function () {
+                Highcharts.setOptions({
+                    chart: {
+                        backgroundColor: {
+                            linearGradient: [0,0,500,500],
+                            stops: [
+                                [0, 'rgb(255, 255, 255)'],
+                                [1,'rgb(240,240,255)']
+                            ]
+                        },
+                        borderWidth: 2,
+                        plotBackgroundColor: 'rbga(255,255,255, .9)',
+                        plotShadow: true,
+                        plotBorderWidth: 1
+                    }
+                });
 
+                var chart1 = new Highcharts.Chart({
+                    chart: {
+                        renderTo:'container',
+                    },
+
+                    xAxis: {
+                        type: 'datetime'
+                    },
+
+                    series: [{
+                        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+                        pointStart: Date.UTC(2018,1,5),
+                        pointInterval: 9600 * 1000 // time interval
+                    }]
+                });
+
+                var chart2 = new Highcharts.Chart({
+                   chart: {
+                       renderTo: 'container2',
+                       type: 'column'
+                   },
+                   xAxis: {
+                       type: 'datetime'
+                   },
+                   series: [{
+                       data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+                       pointStart: Date.UTC(2010,0, 1),
+                       pointInterval: 3600 * 1000 // one hour
+                   }]
+                });
+            });
         </script>
-
     </body>
 </html>
